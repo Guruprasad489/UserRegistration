c69cbaf3bf10abcef3284b1a642d22fd4dbab8c3
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-11 11:28:52 +0530

    [Guru] Added Email Validation method UC3

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -12,9 +12,24 @@ namespace UserRegistration
         {
             Console.WriteLine("WElcome to User Registration Program using Regex\n");
 
-            UserReg.ValidateFirstName();
-            UserReg.ValidateLastName();
-
+            Console.WriteLine("Please choose an option");
+            Console.WriteLine("\n1. First Name \n2. Last Name \n3. Email-ID");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    UserReg.ValidateFirstName();
+                    break;
+                case 2:
+                    UserReg.ValidateLastName();
+                    break;
+                case 3:
+                    UserReg.ValidateEmail();
+                    break;
+                default:
+                    Console.WriteLine("Please choose a valid option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
--- UserRegistration/UserReg.cs
+++ UserRegistration/UserReg.cs
@@ -15,7 +15,7 @@ namespace UserRegistration
             Console.WriteLine("\nPlease Enter your First Name:");
             Console.WriteLine("Note: First name starts with Cap and has minimun 3 characters");
             string firstName = Console.ReadLine();
-            string firstName_pattern = "^[A-Z][aA-zZ]{2,}$";
+            string firstName_pattern = "^[A-Z][a-zA-Z]{2,}$";
 
             if (Regex.IsMatch(firstName, firstName_pattern))
             {
@@ -34,7 +34,7 @@ namespace UserRegistration
             Console.WriteLine("\nPlease Enter your Last Name:");
             Console.WriteLine("Note: Last name starts with Cap and has minimun 3 characters");
             string lastName = Console.ReadLine();
-            string lastName_pattern = "^[A-Z][aA-zZ]{2,}$";
+            string lastName_pattern = "^[A-Z][a-zA-Z]{2,}$";
 
             if (Regex.IsMatch(lastName, lastName_pattern))
             {
@@ -44,6 +44,25 @@ namespace UserRegistration
             {
                 Console.WriteLine("Last Name is not Valid");
                 ValidateLastName();
+            }
+        }
+
+        //UC3
+        public static void ValidateEmail()
+        {
+            Console.WriteLine("\nPlease Enter your Email-ID:");
+            Console.WriteLine("Note: E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions");
+            string email = Console.ReadLine();
+            string email_pattern = @"^[a-zA-Z0-9]{3,10}([._+-][a-zA-Z0-9]{1,10})?@[a-zA-Z0-9]{1,10}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$";
+
+            if (Regex.IsMatch(email, email_pattern))
+            {
+                Console.WriteLine("Email is Valid"); 
+            }
+            else
+            {
+                Console.WriteLine("Email is not Valid");
+                ValidateEmail();
             }
         }
     }
